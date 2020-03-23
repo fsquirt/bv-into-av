f664b0c0859724788a9b1c5e60f0429b8f9a869e
fsquirt <59900359+fsquirt@users.noreply.github.com>
2020-03-24 00:14:21 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import re
+from urllib import request
+
+BV = input('请输入BV号:')
+BVH = str(BV)
+
+url = 'https://api.bilibili.com/x/web-interface/view?bvid='+BVH
+req = request.Request(url)
+page = request.urlopen(req).read()
+page = page.decode('utf-8')
+string = page
+
+print("=====分====割====线=====")
+print('你要查询的BV号是:',BVH)
+
+aidnum = re.findall('"aid":([0-9]*)',string,flags=0)
+av = int(aidnum[0])
+print('此视频AV号为:' + "AV",av)
+print("")
+print("程序制作者:fsquirt,UID:349511867")
+print("感谢你的使用")
+
+input()
+
+
+
+
+
